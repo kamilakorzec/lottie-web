c115b11364745d6f16117f93dcd234c7b2c43ae5
Kamila Korzec <kamila.korzec@yougov.com>
2018-03-29 10:54:18 +0200

    Remove unnecessary files

--- build/player/lottie.js
+++ build/player/lottie.js
@@ -1,4 +1,4 @@
-(typeof navigator !== "undefined") && (function(root, factory) {
+typeof navigator !== "undefined" && (function(root, factory) {
     if (typeof define === "function" && define.amd) {
         define(function() {
             return factory(root);
